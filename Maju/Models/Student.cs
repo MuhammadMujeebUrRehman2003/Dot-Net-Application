808913db562fd1dd86028f7614d71da4ca5c9461
MUJEEB UR REHMAN <MUJEEB UR REHMAN@DESKTOP-71ON71H>
2024-03-24 02:06:37 -0700

    Add project files.

---
+++ Maju/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Maju.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Program { get; set; }
+
+    public string? Department { get; set; }
+
+    public int? Fee { get; set; }
+}
